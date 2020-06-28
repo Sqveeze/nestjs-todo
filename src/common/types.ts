52777a91bf4ebde8954e089e4adfadd9d3b353f6
Márk Pap <mark.pap@finnoconsult.at>
2020-06-28 21:37:36 +0200

    :recycle: types

---
+++ src/common/types.ts
@@ -0,0 +1,31 @@
+export interface IAuthSignUp {
+  username: string;
+  password: string;
+  age: number;
+}
+
+export interface IAuthSignIn {
+  username: string;
+  password: string;
+}
+
+export interface IAuthResult {
+  accessToken: string;
+}
+
+export interface IUser {
+  username: string;
+  password: string;
+  age: number;
+}
+
+export interface ITodo {
+  title: string;
+  text: string;
+  userId?: number;
+}
+
+export interface ITodoItem {
+  text: string;
+  todoId?: number;
+}
