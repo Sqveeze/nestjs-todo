f4adb671f77a9e311bee0ea757b23eb7ca41f82d
Márk Pap <mark.pap@finnoconsult.at>
2020-06-28 21:37:13 +0200

    :sparkles: auth guard (dummy)

---
+++ src/auth.guard.ts
@@ -0,0 +1,20 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const ctx = context.switchToHttp();
+    const request = ctx.getRequest<Request>();
+
+    if (
+      !request.headers.authorization
+      || request.headers.authorization !== 'dummy-access-token'
+    ) return false;
+
+    return true;
+  }
+}
